1ae9adf455e20ef063edd94e1582fb651f41a86b
teiwiet <omegalmao1.3@gmail.com>
2024-03-30 14:41:28 -0400

    update

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include<linux/module.h>
 #include<linux/fs.h>
 #include<linux/init.h>
+#include<linux/cdev.h>
 #define DEV_MEM_SIZE 512
 char device_buffer[DEV_MEM_SIZE];
+
 dev_t device_number;
+
+struct cdev pcd_cdev;
+
+struct file_operations pcd_fops;
 
 
 static int __init pcd_driver_init(void){
+	// dynamically allocate a device number
 	alloc_chrdev_region(&device_number,0,1,"pcd");
+	// initialize the cdev structure with pcd_fops
+	cdev_init(&pcd_cdev,&pcd_fops);
+	pcd_cdev.owner = THIS_MODULE;
+	cdev_add(&pcd_cdev,device_number,1);
 	return 0;
 }
 

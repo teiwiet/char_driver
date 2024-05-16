f332f856a754b8e12cf92b0391a123526192e7de
teiwiet <omegalmao1.3@gmail.com>
2024-05-16 09:11:54 -0400

    update

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ struct cdev pcd_cdev;
 
 struct file_operations pcd_fops;
 
-
 static int __init pcd_driver_init(void){
 	// dynamically allocate a device number
 	alloc_chrdev_region(&device_number,0,1,"pcd");
@@ -22,11 +21,9 @@ static int __init pcd_driver_init(void){
 	return 0;
 }
 
-
 static void __exit pcd_driver_exit(void){
 	
 }
-
 
 module_init(pcd_driver_init);
 module_exit(pcd_driver_exit);

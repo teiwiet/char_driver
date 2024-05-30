50377feab7d3b64610e4873871a64775a31dbb0f
teiwiet <omegalmao1.3@gmail.com>
2024-05-30 09:05:38 +0100

    update

--- main.c
+++ main.c
@@ -15,7 +15,14 @@ loff_t pcd_llseek(struct file* filp,loff_t f_pos,int whence){
 }
 
 ssize_t pcd_read(struct file* filp,char* __user buff,size_t count,loff_t* f_pos){
-	return 0;
+	if(count+*f_pos > MEM_SIZE){
+		count = MEM_SIZE - *f_pos;	
+	}
+	if(copy_to_user(buff,&device_buffer[],count)){
+		return -EFAULT;
+	}
+	*f_pos += count;
+	return count;
 }
 
 ssize_t pcd_write(struct file* filp,const char* __user buff,size_t count,loff_t* f_pos){
@@ -39,21 +46,26 @@ struct file_operations pcd_fops = {
 	.release = pcd_release
 };
 
-
-
+struct class* pcd_class;
+struct device* pcd_device;
 
 static int __init pcd_init(void){
 	ret = alloc_chrdev_region(&device_number,0,1,"pcd_device");
+	pr_info("The driver initialize with %d:%d",MAJOR(device_number),MINOR(device_number));
 	cdev_init(&pcd_cdev,&pcd_fops);
 	pcd_cdev.owner = THIS_MODULE;
 	cdev_add(&pcd_cdev,device_number,1);
-	pr_info(KERN_INFO "what the fuck i didn't print any thing :-))");
+	pcd_class = class_create("pcd_class");
+	pcd_device = device_create(pcd_class,NULL,device_number,NULL,"pcd");
 	return 0;
 }
 
 
 static void __exit pcd_exit(void){
-
+	device_destroy(pcd_class,device_number);
+	class_destroy(pcd_class);
+	cdev_del(&pcd_cdev);
+	unregister_chrdev_region(device_number,1);
 };
 
 

e0c475cd3a6ad851d43f90ea72a298a2a25cf687
teiwiet <omegalmao1.3@gmail.com>
2024-08-23 08:22:30 -0400

    update

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include<linux/init.h>
 #include<linux/module.h>
+#include<linux/fs.h>
+#include<linux/kernel.h>
+static int __init module_init(){
+	return 1;
+}
 
+static void __exit module_exit(){
 
-module_init();
-module_exit();
-MODULE_AUTHOR();
+}
+
+module_init(module_init);
+module_exit(module_exit);
+MODULE_AUTHOR("teiwiet");
+MODULE_DESCRIPTION("Simple kernel driver by teiwiet");
+MODULE_LICENSE("GPL");
